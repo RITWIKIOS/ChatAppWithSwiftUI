a83f55252bbff8ffc878b2d9b68b07d193639e03
Ritwik Ghosh <ritwikghosh22@gmail.com>
2024-05-28 15:23:44 +0530

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Moumita Paul Ghosh on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
